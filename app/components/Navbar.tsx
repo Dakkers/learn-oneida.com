4ffa3bf5a51a75e95f88edcf3b59a9f0f2006d16
Belle Pensamiento <37554274+pensamint@users.noreply.github.com>
2024-03-19 19:24:18 -0400

    first pull request yipeeee (#3)
    * added about page and readme
    
    * formatted code
    
    w0w

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -89,6 +89,14 @@ function NavbarMobile() {
                 <NavigationMenuItem>
                   <NavigationMenuLink
                     className={navigationMenuTriggerStyle()}
+                    href="/about"
+                  >
+                    About
+                  </NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <NavigationMenuLink
+                    className={navigationMenuTriggerStyle()}
                     href="/resources"
                   >
                     Resources
@@ -126,6 +134,15 @@ function NavbarDesktop() {
         <NavigationMenuItem>
           <NavigationMenuLink className={navigationMenuTriggerStyle()} href="/">
             Home
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuLink
+            className={navigationMenuTriggerStyle()}
+            href="/about"
+          >
+            About
           </NavigationMenuLink>
         </NavigationMenuItem>
 
--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,3 +1,7 @@
+import { Flex } from "@/design/components/flex";
+import { Heading } from "@/design/components/heading";
+import { List } from "@/design/components/list";
+import { Text } from "@/design/components/text";
 import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
@@ -9,8 +13,56 @@ export const meta: MetaFunction = () => {
 
 export default function About() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>About</h1>
-    </div>
+    <Flex direction="column" gap={4}>
+      <Heading level={1} variant="headlineL">
+        About Learn-Oneida
+      </Heading>
+
+      <Text>
+        Learn-Oneida was created from Fanshawe College's Oneida Language
+        Immersion, Culture and Teaching Program Curriculum. It was originally
+        adapted from the Twatati Program, designed by and delivered in the
+        Oneida community.
+      </Text>
+
+      <Text>
+        It is produced by Anowal Software, Indigenous-owned, and always
+        maintained with community input in mind.
+      </Text>
+
+      <Heading level={1} variant="headlineS">
+        Land Acknowledgement
+      </Heading>
+
+      <Text>
+        Anowal Software acknowledges that we live and work on Turtle Island
+        which has been home to many Indigenous Peoples since Time Immemorial.
+        Anowal Software Operates on Land traditionally inhabited and cared for
+        by Anishinabe, Haudenosaunee, Attiwonderonk (Neutral) peoples.
+      </Text>
+
+      <Heading level={1} variant="headlineS">
+        Team
+      </Heading>
+
+      <List>
+        <List.Item>Dakota St. Laurent - Lead Developer</List.Item>
+        <List.Item>Belle Pensamiento - Product Manager and Developer</List.Item>
+      </List>
+
+      <Heading level={1} variant="headlineS">
+        Contact Us
+      </Heading>
+
+      <Text>
+        Questions? Comments? Want to see more features? &nbsp;
+        <a
+          className="underline text-blue-600"
+          href="mailto:belle@pensamiento.ca"
+        >
+          Email us!
+        </a>
+      </Text>
+    </Flex>
   );
 }
