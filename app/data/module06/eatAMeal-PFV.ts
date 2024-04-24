75722547f75b1ef263d2590a5d2be0f5ffb1be1a
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2024-04-23 21:21:46 -0400

    feat: change "chew" to "eat" in module 6 [LOC-65] (#14)

---
+++ app/data/module06/eatAMeal-PFV.ts
@@ -0,0 +1,57 @@
+import { createParadigmData } from "~/components/ParadigmTable";
+import { pronouns } from "~/utils";
+
+export default createParadigmData(
+  {
+    translation: "{{pronoun}} {{refVerbPast}} VERB",
+    type: "PB",
+    phrases: [
+      {
+        breakdown: [["wake"], "khuní"],
+      },
+      {
+        breakdown: [["sa"], "khuní"],
+      },
+      {
+        breakdown: [["lo"], "tekhuní"],
+      },
+      {
+        breakdown: [["yako"], "khuní"],
+      },
+      {
+        breakdown: [["yo"], "khuní"],
+      },
+      {
+        breakdown: [["yukni"], "khuní"],
+      },
+      {
+        breakdown: [["yukni"], "khuní"],
+      },
+      {
+        breakdown: [["sni"], "khuní"],
+      },
+      {
+        breakdown: [["loti"], "khuní"],
+      },
+      {
+        breakdown: [["yoti"], "khuní"],
+      },
+      {
+        breakdown: [["yukw"], "atekhuní"],
+      },
+      {
+        breakdown: [["yukwa"], "khuní"],
+      },
+      {
+        breakdown: [["swa"], "khuní"],
+      },
+      {
+        breakdown: [["loti"], "khuní"],
+      },
+      {
+        breakdown: [["yoti"], "khuní"],
+      },
+    ],
+  },
+  pronouns
+);
