63df64780c382be1bba0cec5388fec1e7407fa80
pensamint <belle@pensamiento.ca>
2024-05-04 19:12:36 -0400

    temp

---
+++ app/utils/words.tests.ts
@@ -0,0 +1,22 @@
+import { describe, it } from "node:test";
+import assert from "node:assert";
+import { whisperizeWord } from "./words";
+import { unwhisperWord } from "./words";
+import { removeAccents } from "./words";
+import { removeWhisper } from "./words";
+import { removeGlottalStop } from "./words";
+import { removeLongStress } from "./words";
+import { sanitizeIrregularCharacters } from "./words";
+
+describe("whisperizeWord()", () => {
+// cases
+
+// 
+
+});
+
+describe("unwhisperWord()", () => {
+  // cases
+  
+});
+  
