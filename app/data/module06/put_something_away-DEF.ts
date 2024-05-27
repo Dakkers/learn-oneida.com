22d4f18118132cd4c5e861a6f121617eff406eb0
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2024-05-27 07:12:37 -0400

    fix: small typo fixes [LOC-0]

--- app/data/module06/put_something_away-DEF.ts
+++ app/data/module06/put_something_away-DEF.ts
@@ -7,49 +7,49 @@ export default createParadigmData(
     type: "PR",
     phrases: [
       {
-        breakdown: [["waʔ", "DEF"], ["k"], "atewyʌ·tú"],
+        breakdown: [["waʔ", "DEF"], ["k"], "atewyʌ́·tuʔ"],
       },
       {
-        breakdown: [["wa", "DEF"], ["hs"], "atewyʌ·tú"],
+        breakdown: [["wa", "DEF"], ["hs"], "atewyʌ́·tuʔ"],
       },
       {
-        breakdown: [["wa", "DEF"], ["h"], "atewyʌ·tú"],
+        breakdown: [["wa", "DEF"], ["h"], "atewyʌ́·tuʔ"],
       },
       {
-        breakdown: [["waʔ", "DEF"], ["u"], "tewyʌ·tú"],
+        breakdown: [["waʔ", "DEF"], ["u"], "tewyʌ́·tuʔ"],
       },
       {
-        breakdown: [["waʔ", "DEF"], ["w"], "atewyʌ·tú"],
+        breakdown: [["waʔ", "DEF"], ["w"], "atewyʌ́·tuʔ"],
       },
       {
-        breakdown: [["we", "DEF"], ["ty"], "atewyʌ·tú"],
+        breakdown: [["we", "DEF"], ["ty"], "atewyʌ́·tuʔ"],
       },
       {
-        breakdown: [["waʔ", "DEF"], ["aky"], "atewyʌ·tú"],
+        breakdown: [["waʔ", "DEF"], ["aky"], "atewyʌ́·tuʔ"],
       },
       {
-        breakdown: [["we", "DEF"], ["tsy"], "atewyʌ·tú"],
+        breakdown: [["we", "DEF"], ["tsy"], "atewyʌ́·tuʔ"],
       },
       {
-        breakdown: [["wa", "DEF"], ["hy"], "atewyʌ·tú"],
+        breakdown: [["wa", "DEF"], ["hy"], "atewyʌ́·tuʔ"],
       },
       {
-        breakdown: [["waʔ", "DEF"], ["ky"], "atewyʌ·tú"],
+        breakdown: [["waʔ", "DEF"], ["ky"], "atewyʌ́·tuʔ"],
       },
       {
-        breakdown: [["we", "DEF"], ["tw"], "atewyʌ·tú"],
+        breakdown: [["we", "DEF"], ["tw"], "atewyʌ́·tuʔ"],
       },
       {
-        breakdown: [["waʔ", "DEF"], ["akw"], "atewyʌ·tú"],
+        breakdown: [["waʔ", "DEF"], ["akw"], "atewyʌ́·tuʔ"],
       },
       {
-        breakdown: [["we", "DEF"], ["sw"], "atewyʌ·tú"],
+        breakdown: [["we", "DEF"], ["sw"], "atewyʌ́·tuʔ"],
       },
       {
-        breakdown: [["wa", "DEF"], ["hu"], "tewyʌ·tú"],
+        breakdown: [["wa", "DEF"], ["hu"], "tewyʌ́·tuʔ"],
       },
       {
-        breakdown: [["waʔ", "DEF"], ["ku"], "tewyʌ·tú"],
+        breakdown: [["waʔ", "DEF"], ["ku"], "tewyʌ́·tuʔ"],
       },
     ],
     whispered: false,
