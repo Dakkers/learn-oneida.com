6a2ffcbf223f90f0d6f5ce653c09076dbcc09e4b
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2024-06-15 16:22:51 -0400

    some fixes

--- packages/ohutsya/src/components/Card/Card.stories.tsx
+++ packages/ohutsya/src/components/Card/Card.stories.tsx
@@ -1,9 +1,11 @@
-import type { Meta, StoryFn } from '@storybook/react';
-import { Card } from './Card';
+import type { Meta, StoryFn } from "@storybook/react";
+import { Card } from "./Card";
 
 export default {
-  title: 'Components/Card',
+  title: "Components/Card",
   component: Card,
 } satisfies Meta<typeof Card>;
 
-export const Basic: StoryFn<typeof Card> = (args) => <Card {...args}>Card</Card>
+export const Basic: StoryFn<typeof Card> = (args) => (
+  <Card {...args}>Card</Card>
+);
--- packages/ohutsya/src/components/Card/Card.tsx
+++ packages/ohutsya/src/components/Card/Card.tsx
@@ -3,20 +3,17 @@ import * as React from "react";
 import { cn } from "../../utils/tailwind";
 
 interface CardProps {
-  as?: 'section' | 'article';
+  as?: "section" | "article";
 
   children: React.ReactNode;
 }
 
-export function Card ({
-  as: Tag = 'article',
-  children
-}: CardProps) {
+export function Card({ as: Tag = "article", children }: CardProps) {
   return (
     <Tag
       className={cn("rounded-lg border bg-card text-card-foreground shadow-sm")}
     >
-      <div className={cn('p-6')}>{children}</div>
+      <div className={cn("p-6")}>{children}</div>
     </Tag>
   );
 }
@@ -25,10 +22,6 @@ interface CardFooterProps {
   children: React.ReactNode;
 }
 
-Card.Footer = function CardFooter ({ children }: CardFooterProps) {
-  return (
-    <footer>
-      {children}
-    </footer>
-  )
-}
+Card.Footer = function CardFooter({ children }: CardFooterProps) {
+  return <footer>{children}</footer>;
+};
