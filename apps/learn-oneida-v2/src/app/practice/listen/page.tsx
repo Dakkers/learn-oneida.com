7229446bcc3293752bc8cddf460bae01830f9fec
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2024-08-03 13:04:47 -0400

    fin?

--- apps/learn-oneida-v2/src/app/practice/listen/page.tsx
+++ apps/learn-oneida-v2/src/app/practice/listen/page.tsx
@@ -1,11 +1,29 @@
 "use client";
 
-import React, { useState } from "react";
-import { SectionHeading } from "@ukwehuwehneke/language-components";
-import { Box, Button, Flex, Select, Text } from "@ukwehuwehneke/ohutsya";
+import React, {
+  RefObject,
+  useCallback,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import {
+  translatePhrase,
+  SectionHeading,
+} from "@ukwehuwehneke/language-components";
+import { Box, Button, Card, Flex, Select, Text } from "@ukwehuwehneke/ohutsya";
 import _ from "lodash";
 import { getParticlesForGroup } from "@/components/articles/ParticlesTable";
 import { flushSync } from "react-dom";
+import { getTranslationExercisesForModule } from "@/components/practice/TranslationExercises";
+import WaveSurfer from "wavesurfer.js";
+import WavesurferPlayer, { WavesurferProps } from "@wavesurfer/react";
+import { createModule4Data } from "@/data/module04";
+import {
+  createTimesOfDayData,
+  determineTimesOfDayAudioFileName,
+} from "@/components/articles/TimesOfDay";
+import { arrayify } from "@/utils";
 
 const meta: any = () => {
   return [
@@ -17,11 +35,22 @@ const meta: any = () => {
   ];
 };
 
+type Data = Array<{
+  audioFile: string;
+  en: string;
+  translation: string;
+}>;
+
 export default function PracticeListening() {
-  const categories = [
+  const categories: Array<{
+    getData: () => Data;
+    label: string;
+    sub?: { label: string; value: string }[];
+    value: string;
+  }> = [
     {
       getData: () => {
-        return;
+        return setupModule4Data(["here"]);
       },
       label: "Being somewhere",
       sub: [
@@ -31,10 +60,10 @@ export default function PracticeListening() {
       ],
       value: "being_somewhere",
     },
-    {
-      label: "Living somewhere",
-      value: "living_somewhere",
-    },
+    // {
+    //   label: "Living somewhere",
+    //   value: "living_somewhere",
+    // },
     {
       getData: () => {
         const data = getParticlesForGroup(subcategory);
@@ -54,105 +83,313 @@ export default function PracticeListening() {
       value: "particles",
     },
     {
+      getData: () => {
+        const result = [];
+        const data = createTimesOfDayData();
+
+        for (const key in data) {
+          const phrases = data[key as keyof typeof data];
+          for (const p of phrases) {
+            const translations = arrayify(p.translation);
+            for (let i = 0; i < translations.length; i++) {
+              const t = translations[i];
+              result.push({
+                audioFile: `/module04/time_phrases/${determineTimesOfDayAudioFileName(
+                  p.en,
+                  p.translation,
+                  i,
+                )}.mp3`,
+                en: arrayify(p.en).join("; "),
+                translation: t,
+              });
+            }
+          }
+        }
+        return result;
+      },
       label: "Times of day",
       value: "times_of_day",
     },
     {
+      getData: () => {
+        const data = getTranslationExercisesForModule(subcategory);
+        return data.map((datum) => ({
+          audioFile: `/translation_exercises/${subcategory}/ex_${datum[0]}.mp3`,
+          en: "",
+          translation: datum[1],
+        }));
+      },
       label: "Translation exercises",
       sub: [
-        { label: "All", value: "all" },
-        { label: "Module 1", value: "m01" },
+        // { label: "All", value: "all" },
+        { label: "Module 1", value: "module01" },
+        { label: "Module 6", value: "module06" },
       ],
       value: "exercises",
     },
     {
+      getData: () => {
+        return setupModule4Data(["thought"]);
+      },
       label: "Thought",
       value: "thought",
     },
     {
+      getData: () => {
+        return setupModule4Data(["want"]);
+      },
       label: "Wanting something to happen",
       value: "wanting",
     },
   ];
 
-  const [category, setCategory] = useState(categories[0].value);
-  const [subcategory, setSubcategory] = useState(
-    categories[0].sub?.[0].value ?? "",
-  );
+  const [category, setCategory] = useState("");
+  const [subcategory, setSubcategory] = useState("");
   const [index, setIndex] = useState(0);
-  const [hasStarted, setHasStarted] = useState(false);
-  const [data, setData] = useState([]);
+  const [isShowingAnswer, setIsShowingAnswer] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const wavesurferRef = useRef<{ value: WaveSurfer | null }>({ value: null });
 
   const selectedCategory = categories.find((c) => c.value === category);
+  const subcategoryOptions = (selectedCategory?.sub ?? []).map((c) => ({
+    label: c.label,
+    value: c.value,
+  }));
 
-  const playAudioAtIndex = (datum: any) => {
-    const audio = new Audio(`/audio${datum.audioFile}`);
-    audio.play();
+  const updateIndex = (index: number) => {
+    setIndex(index);
+    setIsShowingAnswer(false);
   };
+
+  const data = useMemo(() => {
+    if (!category || !selectedCategory) {
+      return [];
+    }
+    return _.shuffle(selectedCategory.getData());
+  }, [category, subcategory]);
+
+  const currentDatum = data[index];
 
   return (
     <>
       <SectionHeading level={1}>Listening Practice</SectionHeading>
 
-      {hasStarted ? (
-        <>
-          <Text>
-            {index + 1} of {data.length}
-          </Text>
+      <Flex direction="column" gap={6}>
+        <Select
+          label="Category"
+          options={[
+            {
+              label: "(Select)",
+              value: "",
+            },
+            ...categories.map((c) => ({
+              label: c.label,
+              value: c.value,
+            })),
+          ]}
+          onChange={(newVal) => {
+            setCategory(newVal);
+            const newCat = categories.find((c) => c.value === newVal);
+            setSubcategory(newCat?.sub?.[0].value ?? "");
+            setIndex(0);
+            setIsShowingAnswer(false);
+          }}
+          value={category}
+        />
+        {subcategoryOptions.length > 0 && (
+          <Select
+            disabled={!selectedCategory?.sub}
+            label="Subcategory"
+            options={subcategoryOptions}
+            onChange={(val) => {
+              setSubcategory(val);
+              setIndex(0);
+              setIsShowingAnswer(false);
+            }}
+            value={subcategory}
+          />
+        )}
+      </Flex>
 
-          <Box />
-
-          <Button onClick={() => playAudioAtIndex(data[index])}>
-            Play Audio
-          </Button>
-          <Button onClick={() => setIndex(index + 1)}>Next</Button>
-          <Button onClick={() => window.alert(data[index].translation)}>
-            Show Answer
-          </Button>
-          <Button onClick={() => window.alert(data[index].en)}>
-            Show English
-          </Button>
-        </>
-      ) : (
+      {currentDatum && (
         <>
-          <Flex gap={2}>
-            <Select
-              label="Category"
-              options={categories.map((c) => ({
-                label: c.label,
-                value: c.value,
-              }))}
-              onChange={(newVal) => {
-                setCategory(newVal);
-                const newCat = categories.find((c) => c.value === newVal);
-                setSubcategory(newCat?.sub?.[0].value ?? "");
-              }}
-              value={category}
-            />
-            <Select
-              disabled={!selectedCategory?.sub}
-              label="Subcategory"
-              options={(selectedCategory?.sub ?? []).map((c) => ({
-                label: c.label,
-                value: c.value,
-              }))}
-              onChange={setSubcategory}
-              value={subcategory}
-            />
+          <Box>
+            <Text variant="titleS">
+              Audio track {index + 1} of {data.length}
+            </Text>
+
+            <Box pt={4}>
+              <Player
+                audioFile={`/audio/${currentDatum.audioFile}`}
+                autoplay
+                onPlay={setIsPlaying}
+                wavesurferRef={wavesurferRef}
+              />
+            </Box>
+          </Box>
+
+          <Flex justify="between">
+            <Flex gap={2}>
+              <Button
+                emphasis="fill"
+                onClick={() => wavesurferRef.current?.value?.playPause()}
+              >
+                {isPlaying ? "Pause" : "Play"}
+              </Button>
+              <Button
+                disabled={index === 0}
+                onClick={() => updateIndex(index - 1)}
+              >
+                Prev
+              </Button>
+              <Button
+                disabled={index === data.length - 1}
+                onClick={() => updateIndex(index + 1)}
+              >
+                Next
+              </Button>
+            </Flex>
+
+            <Button onClick={() => setIsShowingAnswer(!isShowingAnswer)}>
+              {isShowingAnswer ? "Hide Answer" : "Show Answer"}
+            </Button>
           </Flex>
 
-          <Button
-            onClick={() => {
-              const newData = _.shuffle(selectedCategory?.getData?.() ?? []);
-              setData(newData);
-              setHasStarted(true);
-              playAudioAtIndex(newData[0]);
-            }}
-          >
-            Start
-          </Button>
+          {isShowingAnswer && (
+            <Card>
+              <b>Oneida</b>: {currentDatum.translation}
+              {!!currentDatum.en && (
+                <Box pt={4}>
+                  <b>English</b>: {currentDatum.en}
+                </Box>
+              )}
+            </Card>
+          )}
         </>
       )}
     </>
   );
+}
+
+function Player({
+  audioFile,
+  autoplay = false,
+  onPlay,
+  wavesurferRef,
+}: {
+  audioFile: string;
+  autoplay?: boolean;
+  onPlay: (value: boolean) => void;
+  wavesurferRef?: RefObject<{ value: WaveSurfer | null }>;
+}) {
+  const [duration, setDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [hasFinishedPlayback, setHasFinishedPlayback] = useState(false);
+  const [prevAudioFile, setPrevAudioFile] = useState(audioFile);
+
+  if (audioFile !== prevAudioFile) {
+    setCurrentTime(0);
+    setPrevAudioFile(audioFile);
+    setHasFinishedPlayback(false);
+  }
+
+  const onReady = useCallback(
+    (ws: WaveSurfer, audioDuration: number) => {
+      if (wavesurferRef?.current) {
+        wavesurferRef.current.value = ws;
+      }
+      onPlay(false);
+      setDuration(audioDuration);
+    },
+    [autoplay],
+  );
+
+  const durationFormatted = useMemo(() => {
+    if (duration === 0) {
+      return "0:01";
+    }
+    return formatSecondsAsTime(duration);
+  }, [duration]);
+
+  const currentTimeFormatted = useMemo(
+    () => formatSecondsAsTime(currentTime),
+    [currentTime],
+  );
+
+  return (
+    <>
+      <WavesurferPlayer
+        autoplay={autoplay}
+        height={100}
+        onReady={onReady}
+        onPlay={() => onPlay(true)}
+        onPause={() => onPlay(false)}
+        onFinish={() => setHasFinishedPlayback(true)}
+        onTimeupdate={(ws, time) => {
+          setCurrentTime(time);
+          setHasFinishedPlayback(false);
+        }}
+        url={audioFile}
+        waveColor="#F3F4F6"
+      />
+
+      <Flex justify="between">
+        <Text>
+          {hasFinishedPlayback ? durationFormatted : currentTimeFormatted}
+        </Text>
+        <Text>{durationFormatted}</Text>
+      </Flex>
+    </>
+  );
+}
+
+function formatSecondsAsTime(value: number) {
+  const seconds = Math.floor(value) % 60;
+  const minutes = Math.floor(value / 60);
+  return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+}
+
+function setupModule4Data(keyGroups: string[]) {
+  const mod4Data = createModule4Data();
+  const isHereKeys = [
+    "data-is-here",
+    "data-was-here",
+    "data-will-be-here",
+    "data-might-be-here",
+    "data-is-not-here",
+    "data-was-not-here",
+    "data-will-not-be-here",
+  ];
+  const thoughtKeys = ["data-thought"];
+  const wantKeys = [
+    "data-want",
+    "data-dont-want",
+    "data-used-to-want",
+    // "data-didnt-used-to-want",
+  ];
+
+  const keysToUse: string[] = [];
+  const someMap: Record<string, string[]> = {
+    here: isHereKeys,
+    thought: thoughtKeys,
+    want: wantKeys,
+  };
+  for (const keyGroup of keyGroups) {
+    keysToUse.push(...(someMap[keyGroup] ?? []));
+  }
+
+  const stuff = mod4Data.filter((group) => keysToUse.includes(group.key));
+
+  const result: Data = [];
+  for (const group of stuff) {
+    for (const datum of group.data.phrases) {
+      const folder = group.key.replace("data-", "").replaceAll("-", "_");
+      result.push({
+        audioFile: `/module04/${folder}/${datum.pronoun}.mp3`,
+        en: translatePhrase(group.data.translation, datum.pronoun),
+        translation: datum.phrase,
+      });
+    }
+  }
+  return result;
 }
--- apps/learn-oneida-v2/src/components/articles/TimesOfDay.tsx
+++ apps/learn-oneida-v2/src/components/articles/TimesOfDay.tsx
@@ -10,6 +10,169 @@ import { ArticleProps } from "./utils";
 import { arrayify } from "@/utils";
 
 export function TimesOfDayArticle({ level = 1 }: ArticleProps) {
+  const data = createTimesOfDayData();
+
+  return (
+    <Flex direction="column" gap={4}>
+      <SectionHeading id="times-of-day" level={level}>
+        Times of day
+      </SectionHeading>
+      <Text>
+        Below is a bunch of phrases related to what time of day it is (not a
+        specific clock time).
+      </Text>
+
+      <SectionHeading
+        id="times-of-day-times"
+        level={(level + 1) as SectionHeadingProps["level"]}
+      >
+        Times
+      </SectionHeading>
+      <TableWrapper columns={columns} data={data.timePhrases} />
+
+      <SectionHeading
+        id="times-of-day-daytime"
+        level={(level + 1) as SectionHeadingProps["level"]}
+      >
+        Day, daytime
+      </SectionHeading>
+      <TableWrapper columns={columns} data={data.dayPhrases} />
+
+      <SectionHeading
+        id="times-of-day-morning"
+        level={(level + 1) as SectionHeadingProps["level"]}
+      >
+        Morning
+      </SectionHeading>
+      <TableWrapper columns={columns} data={data.morningPhrases} />
+
+      <SectionHeading
+        id="times-of-day-afternoon"
+        level={(level + 1) as SectionHeadingProps["level"]}
+      >
+        Afternoon
+      </SectionHeading>
+      <TableWrapper columns={columns} data={data.afternoonPhrases} />
+
+      <SectionHeading
+        id="times-of-day-evening"
+        level={(level + 1) as SectionHeadingProps["level"]}
+      >
+        Evening
+      </SectionHeading>
+      <TableWrapper columns={columns} data={data.eveningPhrases} />
+
+      <SectionHeading
+        id="times-of-day-night"
+        level={(level + 1) as SectionHeadingProps["level"]}
+      >
+        Night
+      </SectionHeading>
+      <TableWrapper columns={columns} data={data.nightPhrases} />
+
+      <SectionHeading
+        id="times-of-day-tomorrow"
+        level={(level + 1) as SectionHeadingProps["level"]}
+      >
+        Tomorrow
+      </SectionHeading>
+      <TableWrapper columns={columns} data={data.tomorrowPhrases} />
+
+      <SectionHeading
+        id="times-of-day-yesterday"
+        level={(level + 1) as SectionHeadingProps["level"]}
+      >
+        Yesterday
+      </SectionHeading>
+      <TableWrapper columns={columns} data={data.yesterdayPhrases} />
+
+      <SectionHeading
+        id="times-of-day-week"
+        level={(level + 1) as SectionHeadingProps["level"]}
+      >
+        Week
+      </SectionHeading>
+      <TableWrapper columns={columns} data={data.weekPhrases} />
+
+      <SectionHeading
+        id="times-of-day-weekend"
+        level={(level + 1) as SectionHeadingProps["level"]}
+      >
+        Weekend
+      </SectionHeading>
+      <TableWrapper columns={columns} data={data.weekendPhrases} />
+
+      <SectionHeading
+        id="times-of-day-month"
+        level={(level + 1) as SectionHeadingProps["level"]}
+      >
+        Months
+      </SectionHeading>
+      <TableWrapper columns={columns} data={data.monthPhrases} />
+
+      <SectionHeading
+        id="times-of-day-year"
+        level={(level + 1) as SectionHeadingProps["level"]}
+      >
+        Years
+      </SectionHeading>
+      <TableWrapper columns={columns} data={data.yearPhrases} />
+
+      <SectionHeading
+        id="times-of-day-other"
+        level={(level + 1) as SectionHeadingProps["level"]}
+      >
+        Other Phrases
+      </SectionHeading>
+      <TableWrapper columns={columns} data={data.otherPhrases} />
+    </Flex>
+  );
+}
+
+const fixAudioFileName = (filename: string) =>
+  filename
+    .replaceAll("(", "")
+    .replaceAll(")", "")
+    .replaceAll("?", "")
+    .replaceAll(" ", "_");
+
+export const determineTimesOfDayAudioFileName = (
+  phrase: string | string[],
+  translations: string | string[],
+  index: number,
+) => {
+  const name = fixAudioFileName(arrayify(phrase)[0]);
+  const hasMultiple = Array.isArray(translations);
+  const suffix = hasMultiple ? `_${index + 1}` : "";
+  return `${name}${suffix}`;
+};
+
+const columns = [
+  TableWrapper.columnsEnglishTranslation[0],
+  {
+    ...TableWrapper.columnsEnglishTranslation[1],
+    cell: (
+      value: string | string[],
+      row: {
+        en: string | string[];
+      },
+    ) => (
+      <Flex direction="column" gap={2}>
+        {arrayify(value).map((val, i) => (
+          <Flex gap={4} key={i}>
+            {val}
+
+            <PlayButton
+              filepath={`/audio/module04/time_phrases/${determineTimesOfDayAudioFileName(row.en, value, i)}.mp3`}
+            />
+          </Flex>
+        ))}
+      </Flex>
+    ),
+  },
+];
+
+export function createTimesOfDayData() {
   const dayPhrases = [
     { en: "day", translation: "awʌhnísla̲ʔ" },
     { en: "all day", translation: "kwʌʔtáti̲ʔ" },
@@ -130,162 +293,20 @@ export function TimesOfDayArticle({ level = 1 }: ArticleProps) {
     { en: "sometimes", translation: "swatye·lʌ́ˍ" },
   ];
 
-  return (
-    <Flex direction="column" gap={4}>
-      <SectionHeading id="times-of-day" level={level}>
-        Times of day
-      </SectionHeading>
-      <Text>
-        Below is a bunch of phrases related to what time of day it is (not a
-        specific clock time).
-      </Text>
-
-      <SectionHeading
-        id="times-of-day-times"
-        level={(level + 1) as SectionHeadingProps["level"]}
-      >
-        Times
-      </SectionHeading>
-      <TableWrapper columns={columns} data={timePhrases} />
-
-      <SectionHeading
-        id="times-of-day-daytime"
-        level={(level + 1) as SectionHeadingProps["level"]}
-      >
-        Day, daytime
-      </SectionHeading>
-      <TableWrapper columns={columns} data={dayPhrases} />
-
-      <SectionHeading
-        id="times-of-day-morning"
-        level={(level + 1) as SectionHeadingProps["level"]}
-      >
-        Morning
-      </SectionHeading>
-      <TableWrapper columns={columns} data={morningPhrases} />
-
-      <SectionHeading
-        id="times-of-day-afternoon"
-        level={(level + 1) as SectionHeadingProps["level"]}
-      >
-        Afternoon
-      </SectionHeading>
-      <TableWrapper columns={columns} data={afternoonPhrases} />
-
-      <SectionHeading
-        id="times-of-day-evening"
-        level={(level + 1) as SectionHeadingProps["level"]}
-      >
-        Evening
-      </SectionHeading>
-      <TableWrapper columns={columns} data={eveningPhrases} />
-
-      <SectionHeading
-        id="times-of-day-night"
-        level={(level + 1) as SectionHeadingProps["level"]}
-      >
-        Night
-      </SectionHeading>
-      <TableWrapper columns={columns} data={nightPhrases} />
-
-      <SectionHeading
-        id="times-of-day-tomorrow"
-        level={(level + 1) as SectionHeadingProps["level"]}
-      >
-        Tomorrow
-      </SectionHeading>
-      <TableWrapper columns={columns} data={tomorrowPhrases} />
-
-      <SectionHeading
-        id="times-of-day-yesterday"
-        level={(level + 1) as SectionHeadingProps["level"]}
-      >
-        Yesterday
-      </SectionHeading>
-      <TableWrapper columns={columns} data={yesterdayPhrases} />
-
-      <SectionHeading
-        id="times-of-day-week"
-        level={(level + 1) as SectionHeadingProps["level"]}
-      >
-        Week
-      </SectionHeading>
-      <TableWrapper columns={columns} data={weekPhrases} />
-
-      <SectionHeading
-        id="times-of-day-weekend"
-        level={(level + 1) as SectionHeadingProps["level"]}
-      >
-        Weekend
-      </SectionHeading>
-      <TableWrapper columns={columns} data={weekendPhrases} />
-
-      <SectionHeading
-        id="times-of-day-month"
-        level={(level + 1) as SectionHeadingProps["level"]}
-      >
-        Months
-      </SectionHeading>
-      <TableWrapper columns={columns} data={monthPhrases} />
-
-      <SectionHeading
-        id="times-of-day-year"
-        level={(level + 1) as SectionHeadingProps["level"]}
-      >
-        Years
-      </SectionHeading>
-      <TableWrapper columns={columns} data={yearPhrases} />
-
-      <SectionHeading
-        id="times-of-day-other"
-        level={(level + 1) as SectionHeadingProps["level"]}
-      >
-        Other Phrases
-      </SectionHeading>
-      <TableWrapper columns={columns} data={otherPhrases} />
-    </Flex>
-  );
+  const result = {
+    dayPhrases,
+    eveningPhrases,
+    monthPhrases,
+    morningPhrases,
+    nightPhrases,
+    timePhrases,
+    tomorrowPhrases,
+    weekendPhrases,
+    weekPhrases,
+    yearPhrases,
+    yesterdayPhrases,
+    afternoonPhrases,
+    otherPhrases,
+  };
+  return result;
 }
-
-const fixAudioFileName = (filename: string) =>
-  filename
-    .replaceAll("(", "")
-    .replaceAll(")", "")
-    .replaceAll("?", "")
-    .replaceAll(" ", "_");
-
-const determineAudioFileName = (
-  phrase: string | string[],
-  translations: string | string[],
-  index: number,
-) => {
-  const name = fixAudioFileName(arrayify(phrase)[0]);
-  const hasMultiple = Array.isArray(translations);
-  const suffix = hasMultiple ? ` ${index + 1}` : "";
-  return `${name}${suffix}`;
-};
-
-const columns = [
-  TableWrapper.columnsEnglishTranslation[0],
-  {
-    ...TableWrapper.columnsEnglishTranslation[1],
-    cell: (
-      value: string | string[],
-      row: {
-        en: string | string[];
-      },
-    ) => (
-      <Flex direction="column" gap={2}>
-        {arrayify(value).map((val, i) => (
-          <Flex gap={4} key={i}>
-            {val}
-
-            <PlayButton
-              filepath={`/audio/module04/time_phrases/${determineAudioFileName(row.en, value, i)}.mp3`}
-            />
-          </Flex>
-        ))}
-      </Flex>
-    ),
-  },
-];
