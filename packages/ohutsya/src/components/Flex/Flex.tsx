6aec1c1b0f5af702b7e819149bac6c471eaa27e9
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2024-11-02 16:39:45 -0400

    whoops

--- packages/ohutsya/src/components/Flex/Flex.tsx
+++ packages/ohutsya/src/components/Flex/Flex.tsx
@@ -26,7 +26,7 @@ export interface FlexProps extends PaddingProps {
   as?: "div" | "span";
   children: React.ReactNode;
   direction?: Direction | Responsive<Direction>;
-  gap?: Gap;
+  gap?: Gap | Responsive<Gap>;
   height?: "fill";
   justify?: JustifyContent;
   wrap?: boolean;
@@ -60,7 +60,7 @@ export function Flex({
           direction,
           (val) => `flex-${val === "column" ? "col" : "row"}`,
         ),
-        gap && gapMap.get(gap),
+        responsiveClassName(gap, "gap-"),
         height === "fill" ? "h-full" : undefined,
         justify && justifyContentMap[justify],
         wrap && "flex-wrap",
