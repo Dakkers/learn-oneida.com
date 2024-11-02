4a89bc4fffc3edaf94a2e3494a9c4cf528af2eb5
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2024-11-02 16:42:40 -0400

    pls stop

--- packages/ohutsya/src/components/Flex/Flex.tsx
+++ packages/ohutsya/src/components/Flex/Flex.tsx
@@ -86,18 +86,6 @@ Flex.Item = function FlexItem({
   return <Tag className={cn(grow && growMap.get(grow))}>{children}</Tag>;
 };
 
-const gapMap = new Map([
-  [0, "gap-0"],
-  [1, "gap-1"],
-  [2, "gap-2"],
-  [3, "gap-3"],
-  [4, "gap-4"],
-  [5, "gap-5"],
-  [6, "gap-6"],
-  [7, "gap-7"],
-  [8, "gap-8"],
-]);
-
 const growMap = new Map([
   [0, "grow-0"],
   [1, "grow"],
