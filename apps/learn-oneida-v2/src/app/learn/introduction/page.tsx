27e5b913e5bf83a90b9a1d48951efa78310a140a
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2024-12-12 14:45:01 -0500

    more work on the intro page

--- apps/learn-oneida-v2/src/app/learn/introduction/page.tsx
+++ apps/learn-oneida-v2/src/app/learn/introduction/page.tsx
@@ -77,7 +77,7 @@ export default function LearnIntroduction() {
           <Letter>yakwa</Letter> - first-person, exclusive, plural
         </List.Item>
         <List.Item>
-          <Letter>hnaná·t</Letter> - the <i>noun root</i> for potato
+          <Letter>hnaná·t</Letter> - the <i>noun root</i> for "potato"
         </List.Item>
         <List.Item>
           <Letter>a</Letter> - joiner vowel
@@ -134,8 +134,8 @@ export default function LearnIntroduction() {
       </div>
 
       <Text>
-        ʌ is called the "caret" (pronounced like the vegetable) or "upside down
-        V". ʔ is called the "glottal stop". Here is how each letter is
+        ʌ is called the "caret" (pronounced like the word "carrot") or "upside
+        down V". ʔ is called the "glottal stop". Here is how each letter is
         pronounced on their own:
       </Text>
 
@@ -223,6 +223,154 @@ export default function LearnIntroduction() {
         using the <Link href="/practice/soundchart">sound chart</Link>. Being
         familiar with these sounds is the first step to learning Oneida.
       </Text>
+
+      <SectionHeading id="terminology" level={2}>
+        Terminology
+      </SectionHeading>
+
+      <Text>
+        Before beginning module 1, there are some terms worth clarifying here.
+      </Text>
+
+      <Text>Almost all words in Oneida are comprised of three components:</Text>
+
+      <List ordered>
+        <List.Item>A root word</List.Item>
+        <List.Item>A pronominal prefix</List.Item>
+        <List.Item>An aspect suffix</List.Item>
+      </List>
+
+      <Text>Below, the first two terms will be discussed.</Text>
+
+      <SectionHeading id="root-words" level={3}>
+        Root Words
+      </SectionHeading>
+
+      <Text>
+        Root words are the core part of a larger word that cannot be broken down
+        any further without losing its meaning. This occurs in a very limited
+        way in English: for example, the words "employ" and "employment". One
+        could consider "employ" to be the root word of "employment".
+      </Text>
+
+      <Text>
+        However, in Oneida, root words are not words you can use on their own in
+        conversation or writing. They must have a pronominal prefix and an
+        aspect suffix. As an example, consider these words:
+      </Text>
+
+      <List>
+        <List.Item>
+          <b>kekhu·níheʔ</b> - I am cooking
+        </List.Item>
+        <List.Item>
+          <b>wahakhu·ní·</b> - he cooked
+        </List.Item>
+        <List.Item>
+          <b>ʌyekhu·ní·</b> - she will cook
+        </List.Item>
+        <List.Item>
+          <b>ahsekhu·ní·</b> - you will cook
+        </List.Item>
+      </List>
+
+      <Text>
+        You may have noticed that each of these words has <Letter>khuni</Letter>{" "}
+        in it. <Letter>khuni</Letter> is the root word that means "cook". To
+        reiterate: you can't use this root word on its own! However, to become a
+        proficient speaker, you need to be able to identify root words in
+        conversation and in writing, and also conjugate root words
+        appropriately.
+      </Text>
+
+      <Text>TODO - Show an example for nouns (nuhs)</Text>
+
+      <SectionHeading id="pronominals" level={3}>
+        Pronominal Prefixes
+      </SectionHeading>
+
+      <Text>
+        Pronominal prefixes, often referred to as simply pronominals, are the
+        components of a larger word that convey who is involved. Using the word
+        <Letter>khuni</Letter> again as an example:
+      </Text>
+
+      <List>
+        <List.Item>
+          <b>kekhu·níheʔ</b> - I am cooking
+        </List.Item>
+        <List.Item>
+          <b>sekhu·níheʔ</b> - you are cooking
+        </List.Item>
+        <List.Item>
+          <b>lakhu·níheʔ</b> - he is cooking
+        </List.Item>
+        <List.Item>
+          <b>yekhu·níheʔ</b> - she is cooking
+        </List.Item>
+      </List>
+
+      <Text>
+        The only difference between each of these words is what occurs before
+        the root word. The pronominals in these words are <Letter>ke</Letter>,{" "}
+        <Letter>se</Letter>, <Letter>la</Letter>, and <Letter>ye</Letter>.
+      </Text>
+
+      <Text>
+        Consider another root word, <Letter>atekhuni</Letter>, which is very
+        similar to the previous root word.
+      </Text>
+
+      <List>
+        <List.Item>
+          <b>katekhu·níheʔ</b> - I am eating
+        </List.Item>
+        <List.Item>
+          <b>satekhu·níheʔ</b> - you are eating
+        </List.Item>
+        <List.Item>
+          <b>latekhu·níheʔ</b> - he is eating
+        </List.Item>
+        <List.Item>
+          <b>yutekhu·níheʔ</b> - she is eating
+        </List.Item>
+      </List>
+
+      <Text>
+        Even though the root words are very similar, the prefixes differ. This
+        is because these root words begin with different letters, which are
+        called <b>stems</b>. There are seven stems: one for each vowel, and one
+        additional one that is used for consonants (called a C-stem). Below are
+        examples of each.
+      </Text>
+
+      <List>
+        <List.Item>
+          <b>A-stem</b>: atekhuni (eat), aluʔtat (shoot), anowʌht (lie)
+        </List.Item>
+        <List.Item>
+          <b>E-stem</b>: ehsak (look for), elh (want, think), ehyahl (remember)
+        </List.Item>
+        <List.Item>
+          <b>I-stem</b>: iheyu (die), itaklaʔ (fall down), itaʔ (sleep)
+        </List.Item>
+        <List.Item>
+          <b>O-stem</b>: ohlok (insert), ohalakw (unhook), okw (take out of
+          water)
+        </List.Item>
+        <List.Item>
+          <b>U-stem</b>: uhew (sweep), unhe (live), uni (make)
+        </List.Item>
+        <List.Item>
+          <b>Λ-stem</b>: ʌshenhu (moan), ʌtol (be difficult to please), ʌtuni
+          (feel lonely)
+        </List.Item>
+        <List.Item>
+          <b>C-stem</b>: kw (pick), nohale (wash), hninu (buy)
+        </List.Item>
+      </List>
+
+      <Text>Root words apply to both nouns and verbs.</Text>
     </>
   );
 }
