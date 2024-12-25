e8f434a9e71771b078825df19969b24b41bc2232
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2024-12-24 20:11:52 -0500

    fix builds but for real?

--- apps/learn-oneida-v2/tailwind.config.ts
+++ apps/learn-oneida-v2/tailwind.config.ts
@@ -1,21 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 
-const ohutsyaTailwindConfig = require('../../packages/ohutsya/tailwind.config')
+const ohutsyaTailwindConfig = require("../../packages/ohutsya/tailwind.config");
+const langComponentsTailwindConfig = require("../../packages/language-components/tailwind.config");
 
 const config = {
   darkMode: ["class"],
   content: [
-    './src/app/**/*.{js,jsx,ts,tsx}',
-    './src/components/**/*.{js,jsx,ts,tsx}',
-    ...ohutsyaTailwindConfig.content.map((path:string) => `../../packages/ohutsya/${path}`)
+    "./src/app/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    ...ohutsyaTailwindConfig.content.map(
+      (path: string) => `../../packages/ohutsya/${path}`,
+    ),
+    ...langComponentsTailwindConfig.content.map(
+      (path: string) => `../../packages/language-components/${path}`,
+    ),
   ],
   prefix: "",
   safelist: [
     ...ohutsyaTailwindConfig.safelist,
+    ...langComponentsTailwindConfig.safelist,
   ],
   theme: ohutsyaTailwindConfig.theme,
-  plugins: [...ohutsyaTailwindConfig.plugins, require("tailwindcss-animate")],
-}
+  plugins: [
+    ...ohutsyaTailwindConfig.plugins,
+    ...langComponentsTailwindConfig.plugins,
+    require("tailwindcss-animate"),
+  ],
+};
 
 // console.log(config)
 
