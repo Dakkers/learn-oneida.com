b73a051ccc36891ecd3da5b4a9afbe1d928c2e00
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2025-02-24 21:24:53 -0500

    fuck it, here we go

--- apps/learn-oneida-v2/src/utils/mixpanel.tsx
+++ apps/learn-oneida-v2/src/utils/mixpanel.tsx
@@ -1,28 +1,37 @@
-'use client'
-import mixpanel from 'mixpanel-browser';
-import { useEffect } from 'react';
+"use client";
+import mixpanel from "mixpanel-browser";
+import { useEffect } from "react";
+import { trackEvent } from "./trackEvent";
+import { usePathname } from "next/navigation";
 
-export function initMixpanel () {
+export function initMixpanel() {
+  console.log(process.env);
   const MIXPANEL_TOKEN = process.env.NEXT_PUBLIC_MIXPANEL_TOKEN;
 
   if (!MIXPANEL_TOKEN) {
-    console.warn('Mixpanel token is missing! Check your .env file.');
+    console.warn("Mixpanel token is missing! Check your .env file.");
     return;
   }
 
   mixpanel.init(MIXPANEL_TOKEN, {
     autotrack: false,
     loaded: (instance) => {
-      console.info('Loaded MixPanel');
-      instance.identify()
-    }
+      console.info("Loaded MixPanel");
+      instance.identify();
+    },
   });
 }
 
-export function Mixpanel () {
+export function Mixpanel() {
+  const pathname = usePathname();
+
   useEffect(() => {
     initMixpanel();
   }, []);
+
+  useEffect(() => {
+    trackEvent("Viewed Page");
+  }, [pathname]);
 
   return null;
 }
