16de357983d5cc21d3d2526294be399e5264bf77
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2025-03-31 13:36:39 -0400

    fix: misc fixes

--- apps/learn-oneida-v2/next.config.mjs
+++ apps/learn-oneida-v2/next.config.mjs
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === "development") {
 const cspHeader = `
     default-src 'self';
     connect-src 'self' *.mixpanel.com;
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://unpkg.com;
     style-src 'self' 'unsafe-inline';
     img-src 'self' blob: data:;
     font-src 'self';
